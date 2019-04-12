c3ad4dc20e65d17ae495b0d6881d9d4b387c53b0
Ryan Burn <ryan.burn@gmail.com>
2019-04-11 18:07:39 -0700

    Remove custom attribute code.

--- src/tracer.cpp
+++ src/tracer.cpp
@@ -9,60 +9,13 @@ namespace python_bridge_tracer {
 namespace {
 struct TracerObject {
   PyObject_HEAD
-  PyObject* attributes_dictionary;
 };
 } // namespace
-
-//--------------------------------------------------------------------------------------------------
-// traverseTracer
-//--------------------------------------------------------------------------------------------------
-static int traverseTracer(TracerObject* self, visitproc visit, void* arg) noexcept {
-  Py_VISIT(self->attributes_dictionary);
-  return 0;
-}
-
-//--------------------------------------------------------------------------------------------------
-// getattr
-//--------------------------------------------------------------------------------------------------
-static PyObject* getattr(TracerObject* self, PyObject* name) noexcept {
-  if (self->attributes_dictionary != nullptr) {
-    PyObject* v = PyDict_GetItemWithError(self->attributes_dictionary, name);
-    if (v != nullptr) {
-      Py_INCREF(v);
-      return v;
-    } else if (PyErr_Occurred()) {
-      return nullptr;
-    }
-  }
-  return PyObject_GenericGetAttr(reinterpret_cast<PyObject*>(self), name);
-}
-
-//--------------------------------------------------------------------------------------------------
-// setattr
-//--------------------------------------------------------------------------------------------------
-static int setattr(TracerObject* self, const char* name, PyObject* value) noexcept {
-    if (self->attributes_dictionary == nullptr) {
-        self->attributes_dictionary = PyDict_New();
-        if (self->attributes_dictionary == nullptr) {
-            return -1;
-        }
-    }
-    if (value == nullptr) {
-        int result = PyDict_DelItemString(self->attributes_dictionary, name);
-        if (result < 0 && PyErr_ExceptionMatches(PyExc_KeyError)) {
-            PyErr_SetString(PyExc_AttributeError,
-                "delete non-existing Example attribute");
-        }
-        return result;
-    }
-    return PyDict_SetItemString(self->attributes_dictionary, name, value);
-}
 
 //--------------------------------------------------------------------------------------------------
 // deallocTracer
 //--------------------------------------------------------------------------------------------------
 static void deallocTracer(TracerObject* self) noexcept {
-  Py_CLEAR(self->attributes_dictionary);
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -99,9 +52,6 @@ static void* toVoidPtr(const T* t) noexcept {
 static PyType_Slot TracerTypeSlots[] = {
     {Py_tp_doc, toVoidPtr("CppBridgeTracer")},
     {Py_tp_dealloc, toVoidPtr(deallocTracer)},
-    {Py_tp_traverse, toVoidPtr(traverseTracer)},
-    {Py_tp_getattro, toVoidPtr(getattr)},
-    {Py_tp_setattr, toVoidPtr(setattr)},
     {Py_tp_methods, toVoidPtr(TracerMethods)},
     {0, nullptr}};
 
