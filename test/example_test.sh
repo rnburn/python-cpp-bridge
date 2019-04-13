2eef24de982ceb66ee249d1ad017239307c8d9b0
Ryan Burn <ryan.burn@gmail.com>
2019-04-13 00:11:09 -0700

    load mocktracer plugin.

--- test/example_test.sh
+++ test/example_test.sh
@@ -1,1 +1,3 @@
+ls external/io_opentracing_cpp/mocktracer
+ls
 PYTHONPATH=$PWD ./3rd_party/cpython/bin/python3 test/t.py
--- test/t.py
+++ test/t.py
@@ -1,4 +1,5 @@
 import bridge_tracer
 
 print(bridge_tracer.Tracer)
-print(bridge_tracer.loadTracer('abc', '123'))
+# print(bridge_tracer.loadTracer('abc', '123'))
+print(bridge_tracer.loadTracer('external/io_opentracing_cpp/mocktracer/libmocktracer_plugin.so', 'test/mocktracer-config.json'))
